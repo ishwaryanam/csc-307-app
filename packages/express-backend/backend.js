827b361d46e6edfa640d291a7b8c41024670b623
ishwaryanam <inamburu@calpoly.edu>
2024-04-17 18:25:41 -0700

    front and backend talking to each other

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
@@ -43,6 +44,7 @@ const findUserByNameAndJob = (name, job) => {
 };
 const findUserById = (id) =>
   users["users_list"].find((user) => user["id"] === id);
+app.use(cors());
 app.use(express.json());
 
 const addUser = (user) => {
